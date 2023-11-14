40cc50ba16fc0d442c9a2bbb5f35336a6d7cdad6
xaisita <54438309+xaisito51@users.noreply.github.com>
2023-11-14 23:25:55 +0100

    Logger + comment added to Propped.java (Jiayi)

--- src/basenostates/Propped.java
+++ src/basenostates/Propped.java
@@ -1,30 +1,55 @@
 package basenostates;
 
-public class Propped extends DoorState{
-  /*DoorState type where a User can do actions:
-   * - Close
-   Passes to Locked state after that*/
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-  public Propped(Door door){
+public class Propped extends DoorState {
+  /*DoorState type where a User can do actions:
+   * - Close: The door turn into Locked state after that
+   * This is the only action the User can do with this state, but very important to be there,
+   * because the only way to reset the Door state into a "normal" state (Locked) is closing it.
+   * */
+
+  Logger logger = LoggerFactory.getLogger("basenostates.DoorState.Propped");
+  public Propped(Door door) {
     super(door);
     name = "propped";
   }
-  public String getName(){return name;}
-  public void open(){ //User tries to open a locked door. Unlock first. *
-    System.out.println("Door " + door.getId() + " is propped. You can only close it");
+
+  public String getName() {
+    return name;
   }
-  public void close(){ //User tries to close a locked and closed door *
+
+  public void open() {
+    //System.out.println("Door " + door.getId() + " is propped. You can only close it");
+    logger.info("Door " + door.getId() + " is propped, only can be closed");
+  }
+
+  public void close() {
+    /*User tries to close the Propped Door and it turn into Locked state after closing door*/
     door.setClosed(true);
+    logger.debug("Door " + door.getId() + " isClosed set into" + door.isClosed());
+    logger.info("Door " + door.getId() + " successfully closed");
     door.setState(new Locked(door));
-    System.out.println("Door " + door.getId() + " successfully closed");
+    logger.debug("Door " + door.getId() + " state set into" + door.getStateName());
+    logger.info("Door " + door.getId() + " successfully locked after closing");
+    //System.out.println("Door " + door.getId() + " successfully closed");
   }
-  public void lock(){ //User tries to lock a lacked door *
-    System.out.println("Door " + door.getId() + " is propped. You can only close it");
+
+  public void lock() {
+    /* User tries to lock a propped door */
+    //System.out.println("Door " + door.getId() + " is propped. You can only close it");
+    logger.info("Door " + door.getId() + " is propped, only can be closed");
   }
-  public void unlock(){ //User tries to unlock the door *
-    System.out.println("Door " + door.getId() + " is propped. You can only close it");
+
+  public void unlock() {
+    /* User tries to unlock the door */
+    //System.out.println("Door " + door.getId() + " is propped. You can only close it");
+    logger.info("Door " + door.getId() + " is propped, only can be closed");
   }
-  public void unlock_shortly(){
-    System.out.println("Door " + door.getId() + " is propped. You can only close it");
+
+  public void unlock_shortly() {
+    //System.out.println("Door " + door.getId() + " is propped. You can only close it");
+    logger.info("Door " + door.getId() + " is propped, only can be closed");
   }
 }
