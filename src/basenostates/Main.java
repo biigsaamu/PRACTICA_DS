ae1364e9ad597d7591210a2ed4c9da6b56ea19cb
xaisita <54438309+xaisito51@users.noreply.github.com>
2023-11-14 23:23:06 +0100

    Logger + comment added to Main.java

--- src/basenostates/Main.java
+++ src/basenostates/Main.java
@@ -1,11 +1,19 @@
 package basenostates;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 // Before executing enable assertions :
 // https://se-education.org/guides/tutorials/intellijUsefulSettings.html
 
 public class Main {
 
   public static void main(String[] args) {
+
+    /* DirectoryDoors is merged into DirectoryAreas
+     * DirectoryUsers is merged into DirectoryUserGroup */
+    Logger logger = LoggerFactory.getLogger("basenostates.Main");
+    logger.trace("entering Main");
     //DirectoryDoors.makeDoors();
     DirectoryAreas.makeAreas();
     //DirectoryUsers.makeUsers();
