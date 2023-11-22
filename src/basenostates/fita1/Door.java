ad12378705b288e849f3378cff397a65c9de98fd
xaisita <54438309+xaisito51@users.noreply.github.com>
2023-11-22 12:35:04 +0100

    update comment

--- src/basenostates/fita1/Door.java
+++ src/basenostates/fita1/Door.java
@@ -6,14 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Door {
-  /* Basic unit of the system which Users interact to. The Door should know its own
-   * state (DoorState), and if it is physically closed (boolean).
-   * Users can do actions to the Door according to the Actions class, and the Door should be able
-   * to determine the corresponding DoorState transition by receiving a User request.
-   * Every Door had to know the Areas which it connects, this is needed when a User
-   * wants to access some Area by crossing the corresponding Door, because they had
-   * to validate the access permission.
-   * */
+  // Basic unit of the system which Users interact to. The Door should know its own
+  // state (DoorState), and if it is physically closed (boolean).
+  // Users can do actions to the Door according to the Actions class, and the Door should be able
+  // to determine the corresponding DoorState transition by receiving a User request.
+  // Every Door had to know the Areas which it connects, this is needed when a User
+  // wants to access some Area by crossing the corresponding Door, because they had
+  // to validate the access permission.
 
   final Logger logger = LoggerFactory.getLogger("basenostates.fita1.Door");
   private final String id;
