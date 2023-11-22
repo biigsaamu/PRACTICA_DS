8cda6eca7369b31d888f75469886d4283aa8685d
xaisita <54438309+xaisito51@users.noreply.github.com>
2023-11-22 12:43:24 +0100

    update comment

--- src/basenostates/fita1/Main.java
+++ src/basenostates/fita1/Main.java
@@ -9,13 +9,12 @@ import org.slf4j.LoggerFactory;
 public class Main {
 
   public static void main(String[] args) {
-    /* Logger is incorporated
-     * All instantiation of Areas and Users (UserGroups) is done there,
-     * after initialization WebServer is called to build the simulator API.
-     * as you can se every function responsibility is granted to the corresponding
-     * class Main only calls them to execute and mount the final functional API.
-     * Grasp Pattern: Expert Applied.
-     * */
+    // Logger is incorporated
+    // All instantiation of Areas and Users (UserGroups) is done there,
+    // after initialization WebServer is called to build the simulator API.
+    // as you can se every function responsibility is granted to the corresponding
+    // class Main only calls them to execute and mount the final functional API.
+    // Grasp Pattern: Expert Applied.
 
     Logger logger = LoggerFactory.getLogger("basenostates.fita1.Main");
     logger.trace("entering Main");
